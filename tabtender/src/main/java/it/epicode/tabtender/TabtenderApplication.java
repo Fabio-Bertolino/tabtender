2445f53c7645fad18ce0e04043d9e1df62f7ab58
Fabio-Bertolino <fabiobertix97@gmail.com>
2025-05-13 13:55:56 +0200

    Initial commit.

---
+++ tabtender/src/main/java/it/epicode/tabtender/TabtenderApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.tabtender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabtenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabtenderApplication.class, args);
+	}
+
+}
---
+++ tabtender/src/test/java/it/epicode/tabtender/TabtenderApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.tabtender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabtenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
